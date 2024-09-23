f475e55cd2f38e032ba1aaceef44c7b6ab0f9f47
Pau <2023_pau.lorca@iticbcn.cat>
2024-09-23 17:28:23 +0200

    refactoring fet i començat el desxifrat

--- activitat_01/Rot13.java
+++ activitat_01/Rot13.java
@@ -11,18 +11,19 @@ public class Rot13 {
             'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
 
     public static void main(String[] args) {
-        String prova = "hOlA qUe tAl";
+        String prova = "hola que tal";
+        String prova2 = "HOLA QUE TAL";
 
         String xifrat = xifraRot13(prova);
-
+        
+        System.out.println("String de prova: " + xifraRot13(prova2));
         System.out.println("String de prova: " + xifrat);
-
-
     }
 
 
     /*
-     * Mètode principal que s'encarrega de xifrar una cadena de text
+     * Mètode principal que s'encarrega de xifrar una cadena de text.
+     * Recorre la cadena i xifra cada caràcter
      */
     public static String xifraRot13(String cadena) {
         String xifrat = "";
@@ -33,52 +34,28 @@ public class Rot13 {
             if (!Character.isLetter(caracter) ) {
                 xifrat = xifrat + caracter;
             } else if (Character.isUpperCase(caracter)) {
-                xifrat = xifrat + xifraMajuscula(caracter);
-            } else  xifrat = xifrat + xifraMinuscula(caracter);
+                xifrat = xifrat + xifraLletra(caracter, majuscules);
+            } else  xifrat = xifrat + xifraLletra(caracter, minuscules);
         }
         return xifrat;
-    } 
-
-    /*
-     * Mètode que utilitzarà "xifraRot13(String)" per xifrar minuscules
-     */
-    public static char xifraMinuscula(char caracter) {
-        int desplasament = 13;
-        int posicioInicial = 0;
-        char aRetornar;
-        //Trobem la posició del caracter al arrat+y
-        for (int i = 0; i < minuscules.length; i++) {
-            if (minuscules[i] == caracter) {
-                posicioInicial = i;
-            }
-        }
-        if ((posicioInicial + desplasament) < minuscules.length) {
-            aRetornar = minuscules[posicioInicial + desplasament];
-        } else {
-            int restant = posicioInicial + desplasament - minuscules.length;
-            aRetornar = minuscules[restant];
-        }
-        return aRetornar;
     }
-    
-    /*
-     * Mètode que utilitzarà "xifraRot13(String)" per xifrar minuscules
-     */
-    public static char xifraMajuscula(char caracter) {
+
+    public static char xifraLletra(char caracter, char[] abc) {
         int desplasament = 13;
         int posicioInicial = 0;
         char aRetornar;
-        //Trobem la posició del caracter al arrat+y
-        for (int i = 0; i < majuscules.length; i++) {
-            if (majuscules[i] == caracter) {
+        //Trobem la posició del caracter al array
+        for (int i = 0; i < abc.length; i++) {
+            if (abc[i] == caracter) {
                 posicioInicial = i;
+                break; 
             }
         }
-        if ((posicioInicial + desplasament) < majuscules.length) {
-            aRetornar = majuscules[posicioInicial + desplasament];
+        if ((posicioInicial + desplasament) < abc.length) {
+            aRetornar = abc[posicioInicial + desplasament];
         } else {
-            int restant = posicioInicial + desplasament - majuscules.length;
-            aRetornar = majuscules[restant];
+            int restant = posicioInicial + desplasament - abc.length;
+            aRetornar = abc[restant];
         }
         return aRetornar;
     }
@@ -87,7 +64,33 @@ public class Rot13 {
      * Mètode principal que s'encarrega de desxifrar una cadena de text ja xifrada
      */
     public static String desxifraRot13(String cadena) {
-        return "a";
+        String xifrat = "";
+        //Troba la lletra
+        for (int i = 0; i < cadena.length(); i++) {
+            char caracter = cadena.charAt(i);
+            
+            if (!Character.isLetter(caracter) ) {
+                xifrat = xifrat + caracter;
+            } else if (Character.isUpperCase(caracter)) {
+                xifrat = xifrat + desxifraLletra(caracter, majuscules);
+            } else  xifrat = xifrat + desxifraLletra(caracter, minuscules);
+        }
+        return xifrat;
+    }
+    
+    public static char desxifraLletra(char caracter, char[] abc) {
+        int desplasament = 13;
+        int posicioInicial = 0;
+        char aRetornar = 'e';
+
+        for (int i = 0; i < abc.length; i++) {
+            if (abc[i] == caracter) {
+                posicioInicial = i;
+                break; 
+            }
+        }
+
+        return aRetornar;
     }
  }
 
