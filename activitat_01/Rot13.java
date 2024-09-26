af6ebbf2bc12e632dc4fe170f7e8f2bb1320c620
Pau <2023_pau.lorca@iticbcn.cat>
2024-09-26 18:22:01 +0200

    activitat Rot13 finalitzada

--- activitat_01/Rot13.java
+++ activitat_01/Rot13.java
@@ -9,18 +9,7 @@ public class Rot13 {
      
     private static final char[] majuscules = {'A', 'À', 'Á', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Í', 'Ï', 'J', 'K', 'L', 'M', 
             'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
-
-    public static void main(String[] args) {
-        String prova = "hola que tal";
-        String prova2 = "HOLA QUE TAL";
-
-        String xifrat = xifraRot13(prova);
-        
-        System.out.println("String de prova: " + xifraRot13(prova2));
-        System.out.println("String de prova: " + xifrat);
-    }
-
-
+    
     /*
      * Mètode principal que s'encarrega de xifrar una cadena de text.
      * Recorre la cadena i xifra cada caràcter
@@ -30,7 +19,6 @@ public class Rot13 {
         //Troba la lletra
         for (int i = 0; i < cadena.length(); i++) {
             char caracter = cadena.charAt(i);
-            
             if (!Character.isLetter(caracter) ) {
                 xifrat = xifrat + caracter;
             } else if (Character.isUpperCase(caracter)) {
@@ -82,19 +70,37 @@ public class Rot13 {
         int desplasament = 13;
         int posicioInicial = 0;
         char aRetornar = 'e';
-
         for (int i = 0; i < abc.length; i++) {
             if (abc[i] == caracter) {
                 posicioInicial = i;
-                break; 
+                break;
             }
         }
-
+        if (posicioInicial - desplasament >= 0) {
+            aRetornar = abc[posicioInicial-desplasament];
+        } else {
+            aRetornar = abc[abc.length - Math.abs(posicioInicial - desplasament)];
+        }
         return aRetornar;
+    }
+
+    public static void main(String[] args) {
+        String prova = "hola que tal";
+        String prova2 = "Próvèm cáràctérs èspecïals úÜ";
+        String prova3 = "ArA BarrejAnt MajúScules i MinUSCUles";
+        String prova4 = "Prova       d'altres ----- carácters?!";
+        String prova5 = "'¡'34567{]   ---";
+
+        String[] proves = {prova, prova2, prova3, prova4, prova5};
+        int i = 1;
+        for (String actual: proves) {
+            System.out.println("==============");
+            System.out.println("PROVA NÚMERO " + i);
+            System.out.println("==============");
+            System.out.println("String de prova xifrada: " + xifraRot13(actual));
+            System.out.println("String de prova desxifrada: " + desxifraRot13(xifraRot13(actual)));
+            i++;
+        }
     }
  }
 
-/*
-* pxúi àçn ciú
-* PXÚI ÀÇN CIÚ
-*/
